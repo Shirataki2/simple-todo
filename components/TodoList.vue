b5927adb76895ec313bc383febd58a426d4f54fd
Shirataki2 <ahahah114514@gmail.com>
2019-08-16 17:12:11 +0900

    Init

---
+++ components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="todolist">
+    <h2>TodoList</h2>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}/Done:{{ todo.done }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: Array
+  }
+}
+</script>
+
+<style scoped>
+#todolist {
+  background-color: #aae;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="todo">
+    <h1>Todo</h1>
+    <TodoList :todos="todos"/>
+  </div>
+</template>
+
+<script>
+import TodoList from '~/components/TodoList.vue'
+
+const getUUID = () => {
+  const R = (length) => {
+    let id = ''
+    for(let i = 0; i < length; i++) {
+      id += Math.floor(Math.random() * 16).toString(16)
+    }
+    return id
+  }
+  return `${R(8)}-${R(4)}-4${R(3)}-${R(4)}-${R(12)}`
+}
+
+export default {
+  components: {
+    TodoList
+  },
+  data() {
+    return {
+      todos: [
+        { id: getUUID(), text: 'Fubuki', done: false},
+        { id: getUUID(), text: 'Shirayuki', done: true},
+        { id: getUUID(), text: 'Hatsuyuki', done: false},
+        { id: getUUID(), text: 'Murakumo', done: false}
+      ]
+    }
+  },
+  mounted() {
+    console.log(this.todos)
+  }
+}
+</script>
+
+<style scoped>
+#todo {
+  background-color: #aea;
+}
+</style>
